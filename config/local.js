9d74ccb9c68bc6ce0de464031e26f7de2e5c5560
paulocerez <paulo.ramirez@web.de>
2023-05-15 13:55:39 +0200

    fix: TypeScript build command

--- config/local.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-require('dotenv').config();
-module.exports = {
-    database: {
-        host: 'localhost',
-        username: 'your_local_db_username',
-        password: 'your_local_db_password',
-    },
-};
